ba3d01acb82a83450669762646917db60560821c
Théo Alloucha <talloucha@stagiaire-humanbooster.com>
2024-10-10 14:35:21 +0200

    First commit

---
+++ src/main/java/fr/hb/bcfirst/BcFirstApplication.java
@@ -0,0 +1,13 @@
+package fr.hb.bcfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BcFirstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/hb/bcfirst/BcFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.hb.bcfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
